b399829c435985e3c0e4441d89d0740ff2aaccb1
somanyrobots <somanyrobots@gmail.com>
2025-07-30 21:39:41 -0700

    initial changes to module.json and module.js
    necessary changes to manifest, creating the new stuff we need in the module js file

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,8 @@
 Hooks.once('init', async function() {
-
+  const label = "3/4 Caster";
+  CONFIG.DND5E.spellcastingTypes.leveled.progression.threeQuarters = {label: label, divisor: 1.33, roundUp: true};
+  CONFIG.DND5E.spellProgression.threeQuarters = label;
+  CONFIG.DND5E.featureTypes.class.subtypes.necromanticRite = "Necromantic Rite";
 });
 
 Hooks.once('ready', async function() {
